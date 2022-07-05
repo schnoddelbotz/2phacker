038f0937f22860af07efb31da8d3f26846a5a80b
schnoddelbotz <jan@hacker.ch>
2022-07-05 22:56:53 +0200

    drop serve - hugo's task - pass through fix ~ in cobra defaults

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -12,18 +14,13 @@ import (
 )
 
 var AppVersion string
-var cfgFile string
+var UserHomeDir string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "2phacker",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -38,18 +35,24 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	rootCmd.PersistentFlags().StringP("docs-dir", "d", "~/TUBE_CONTENT/docs", "HUGO repository checkout to work on")
-	rootCmd.PersistentFlags().StringP("movies-dir", "m", "~/TUBE_CONTENT/movies", "top-level folder with videos (.mp4, .mov)")
-	rootCmd.PersistentFlags().StringP("sounds-dir", "s", "~/TUBE_CONTENT/sounds", "top-level folder with music (.mp3)")
-	rootCmd.PersistentFlags().StringP("images-dir", "i", "~/TUBE_CONTENT/images", "top-level folder with images (.jpg)")
-	rootCmd.PersistentFlags().StringP("webroot", "w", "~/TUBE_WEBROOT/", "Local HUGO output directory") // ? S3 rsync hardlink ...
-	rootCmd.PersistentFlags().StringP("templates", "t", "", "Path to custom templates fo movies, sounds and images; CSS etc.")
+	rootCmd.PersistentFlags().StringP("docs-dir", "d", homePath("TUBE_CONTENT/docs"), "HUGO repository checkout to work on")
+	rootCmd.PersistentFlags().StringP("movies-dir", "m", homePath("TUBE_CONTENT/movies"), "top-level folder with videos (.mp4, .mov)")
+	rootCmd.PersistentFlags().StringP("sounds-dir", "s", homePath("TUBE_CONTENT/sounds"), "top-level folder with music (.mp3)")
+	rootCmd.PersistentFlags().StringP("images-dir", "i", homePath("TUBE_CONTENT/images"), "top-level folder with images (.jpg)")
+	rootCmd.PersistentFlags().StringP("webroot", "w", homePath("TUBE_WEBROOT"), "Local HUGO output directory") // ? S3 rsync hardlink ...
+	rootCmd.PersistentFlags().StringP("templates", "t", "", "Path to custom templates for movies, sounds and images; CSS etc.")
 
 	_ = viper.BindPFlag("webroot", rootCmd.PersistentFlags().Lookup("webroot"))
 
 	replacer := strings.NewReplacer("-", "_")
 	viper.SetEnvKeyReplacer(replacer)
 	viper.SetEnvPrefix("2PH")
+}
+
+func homePath(dir string) string {
+	homeDir, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	return filepath.Join(homeDir, dir)
 }
 
 func initConfig() {
--- cmd/serve.go
+++ cmd/serve.go
@@ -9,8 +9,6 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-
-	"schnoddelbotz/2phacker/webserver"
 )
 
 var httpPort string
@@ -18,16 +16,12 @@ var httpPort string
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A wrapper to hugo serve",
+	Long:  `Runs 2phacker's preprocessing tasks, then runs hugo serve`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("serve called")
-		webserver.Serve(".", viper.GetString("port"))
+		// run build
+		// exec hugo serve
 	},
 }
 
